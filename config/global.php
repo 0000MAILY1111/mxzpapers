82e32d21cacc0174aa85598afbbb019818c6f3e9
Maily Valdez <69325471+0000MAILY1111@users.noreply.github.com>
2022-11-22 16:28:30 -0400

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//IP de la pc servidor de dase de datos
+define ("DB_HOST","localhost");
+
+//NOmbre de la base de datos
+define("DB_NAME","mxz_papers2");
+
+//Usuario de la base de datos
+define("DB_USERNAME","root");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD","");
+
+//Definimos  la codificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nomre del proycto
+define("PRO_NOMBRE","MXZ_LIBRERIA");
+?>
